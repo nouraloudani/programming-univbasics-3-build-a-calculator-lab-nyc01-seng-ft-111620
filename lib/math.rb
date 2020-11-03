30b7cac16e6646dc98bdcab91cdf334df920f7a1
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-03 06:42:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -21,3 +21,14 @@ end
 def square_root(num)
 
 end
+
+- `addition` - Build the method `addition` that adds `num2` to `num1` and returns the result of this calculation
+- `subtraction` - Build the method `subtraction` that subtracts `num2` from
+  `num1` and returns the result of this calculation
+- `multiplication` - Build the method `multiplication` that multiplies `num1`
+  by `num2` and returns the result of this calculation
+- `division` - Build the method `division` that divides `num2` into `num1` and returns the result of this calculation
+- `modulo` - Build the method `modulo` that divides `num2` into `num1` and gives
+  us the _remainder_ of this calculation
+- `square_root` -- Build the method `square_root` that finds the square root of
+  `num` and returns the result
